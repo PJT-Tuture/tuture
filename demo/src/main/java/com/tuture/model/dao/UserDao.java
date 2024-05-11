99639b5345983660cee968aca901072f3b792904
jay45652 <sgcheon@naver.com>
2024-05-11 20:35:27 +0900

    feat: signup User 완성

--- demo/src/main/java/com/tuture/model/dao/UserDao.java
+++ demo/src/main/java/com/tuture/model/dao/UserDao.java
@@ -1,4 +1,7 @@
 package com.tuture.model.dao;
 
+import com.tuture.model.dto.SignUpDto;
+
 public interface UserDao {
+    int insertUser(SignUpDto newUser);
 }
--- demo/src/main/java/com/tuture/service/UserServiceImpl.java
+++ demo/src/main/java/com/tuture/service/UserServiceImpl.java
@@ -1,8 +1,17 @@
 package com.tuture.service;
 
+import com.tuture.model.dao.UserDao;
+import com.tuture.model.dto.SignUpDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService{
+    @Autowired
+    private UserDao userDao;
 
+    @Override
+    public int signupUser(SignUpDto request) {
+        return userDao.insertUser(request);
+    }
 }
