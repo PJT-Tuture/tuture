6a76f1c0239c00f143832c7490ef35abe14b761c
jay45652 <sgcheon@naver.com>
2024-05-13 14:32:28 +0900

    refactor: 필요없는 ErrorCode 삭제

--- demo/src/main/java/com/tuture/global/exception/ErrorCode.java
+++ demo/src/main/java/com/tuture/global/exception/ErrorCode.java
@@ -11,8 +11,6 @@ public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 정보의 사용자를 찾을 수 없습니다."),
 
     /* 400 BAD_REQUEST : 잘못된 요청 */
-    INVALID_LENGTH_NICKNAME(HttpStatus.BAD_REQUEST, "닉네임은 2글자 이상, 10글자 이하만 가능 합니다."),
-    INCLUDE_SPECIAL_CHARACTER_NICKNAME(HttpStatus.BAD_REQUEST, "특수문자는 포함할 수 없습니다."),
     DUPLICATED_NICKNAME(HttpStatus.BAD_REQUEST, "중복된 이메일입니다");
 
     private final HttpStatus httpStatus;
